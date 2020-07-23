7c82d5d6c4bd851f84d1592cd638627df8bd0ccc
Azeem-Haider <azeemhaider0322@gmail.com>
2020-07-24 02:54:37 +0500

    testing

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -40,7 +40,7 @@ exports.getCheckoutSession = catchAsyc(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = await User.find({ email: session.customer_email });
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].amount / 100;
 
   await Booking.create({ user, tour, price });
 };
